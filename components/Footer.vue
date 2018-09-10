c58bc29bc59ed67b58795c5a11f988d39364c7b8
Hwasub Lee <lee@hwasub.com>
2018-09-10 10:50:40 +0900

    Add "Fork me on GitHub" button and legal stuffs

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,19 +3,24 @@
     <div class="container">
       <div class="level is-mobile footcredit">
         <div class="level-left">
-          <p class="p-l-xs level-item"><a href="https://sena.kr" class="has-text-black"><img src="~/assets/svg/kiss-wink.svg" width="16" height="16">&nbsp;sena</a></p>
+          <p class="p-l-xs level-item"><a href="https://sena.kr" class="has-text-black">sena</a></p>
         </div>
         <div class="level-right">
+          <p class="p-r-sm level-item"><a href="https://github.com/hwasub/node-hsdc" target="_blank" class="has-text-black">Fork me on GitHub</a></p>
           <p class="p-r-xs level-item"><a href="/LICENSE.txt" class="has-text-black">{{$t('footer.osl')}}</a></p>
         </div>
       </div>
+    </div>
+    <div class="container m-t-lg">
+      <p class="p-l-sm level-item footcredit">Hearthstone is a trademark or registered trademark of Blizzard Entertainment, Inc., in the U.S. and/or other countries.
+This website is not affiliated with Blizzard Entertainment.</p>
     </div>
   </footer>
 </template>
 
 <style scoped>
 .footcredit {
-  font-size: 9pt;
+  font-size: 11px;
   font-family: 'Tahoma';
 }
 </style>
